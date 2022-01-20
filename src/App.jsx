0060b2620cf0613c007e2bda2573a861c0e93550
ivan-desktop <kovachivanagustin@gmail.com>
2022-01-20 14:22:31 -0300

    funciones de autocompletado pasadas al componente App, autocmpletado tambien disponible para los inputs de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import Busqueda from "./components/busqueda";
-import Header from './components/header.jsx'
+import Header from "./components/header.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-    return (
-        <div className="App">
-            <Header></Header>
-            <Busqueda></Busqueda>
-        </div>
-    )
-}
+	const [autocmpDataClients, setAutocmpDataClients] = useState(false);
+	const [autocmpDataPatentes, setAutocmpDataPatentes] = useState(false);
+	const [autocmpDataAutos, setAutocmpDataAutos] = useState(false);
+
+	const getAll = async () => {
+		try {
+			const res = await (await axios.get(`http://localhost:5000/lista/clientes`)).data;
+			setAutocmpDataClients(res);
+		} catch (e) {
+			setAutocmpDataClients(false);
+			console.log(e);
+		}
+		try {
+			const res = await (await axios.get(`http://localhost:5000/lista/patentes`)).data;
+			setAutocmpDataPatentes(res);
+		} catch (e) {
+			setAutocmpDataPatentes(false);
+			console.log(e);
+		}
+
+		try {
+			const res = await (await axios.get(`http://localhost:5000/lista/autos`)).data;
+			setAutocmpDataAutos(res);
+		} catch (e) {
+			setAutocmpDataAutos(false);
+			console.log(e);
+		}
+	};
+
+	useEffect(getAll, []);
+	return (
+		<div className='App'>
+			<Header></Header>
+			<Busqueda
+				autocmpDataClients={autocmpDataClients}
+				autocmpDataPatentes={autocmpDataPatentes}
+				autocmpDataAutos={autocmpDataAutos}
+			></Busqueda>
+		</div>
+	);
+};
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,16 @@
-import './header.scss';
+import "./header.scss";
 
 function Header() {
+	// UTILIZAR ESTE COMOPONENTE PARA ALMACENAR LOS DATOS, DE ESTA MANERA SE PASARAN COMO PROPS
+	// Y ESTARÁN DISPONIBLES EN TODOS LOS COMPONENTES
 
-  // UTILIZAR ESTE COMOPONENTE PARA ALMACENAR LOS DATOS, DE ESTA MANERA SE PASARAN COMO PROPS
-  // Y ESTARÁN DISPONIBLES EN TODOS LOS COMPONENTES
-
-  return (
-    <header className='header'>
-      <nav className='header-nav'>
-        <ul className='header-ul'>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className='header'>
+			<nav className='header-nav'>
+				<ul className='header-ul'></ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
